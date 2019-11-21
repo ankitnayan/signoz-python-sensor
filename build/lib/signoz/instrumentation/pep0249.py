65c41da3173c684eb18fdf71ed1807bb981d64e1
Ankit Nayan <ankitnayan@gmail.com>
2019-11-21 10:41:26 +0530

    debuging mysql queries

--- build/lib/signoz/instrumentation/pep0249.py
+++ build/lib/signoz/instrumentation/pep0249.py
@@ -35,12 +35,13 @@ class CursorWrapper(wrapt.ObjectProxy):
         self._module_name = module_name
         self._connect_params = connect_params
         self._cursor_params = cursor_params
+        print ("Instrumenting mysql cursor")
 
 
 
     def execute(self, sql, params=None):
         
-        # print ("Executing mysql query -> ", sql_sanitizer(sql))
+        print ("Executing mysql query -> ", sql_sanitizer(sql))
         # query = sql_sanitizer(sql)
         query = "SELECT"
         statsd.increment(REQUEST_COUNT_METRIC_NAME,
@@ -56,7 +57,7 @@ class CursorWrapper(wrapt.ObjectProxy):
         start_time = time.time()
         result = self.__wrapped__.execute(sql, params)
         execution_time = (time.time() - start_time) * 1000
-        # print ("Time taken: ", execution_time)
+        print ("Time taken: ", execution_time)
 
         statsd.histogram(REQUEST_LATENCY_METRIC_NAME,
                 execution_time,
